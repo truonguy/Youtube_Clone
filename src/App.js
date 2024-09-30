b327a95ebd9d4ef43d09d96cf72a7c58338db690
truonguy <truonguy123tx@gmail.com>
2024-09-30 21:04:44 +0700

    Update Layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import { Box } from "@mui/material";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed } from './components';
 
-const App = () => {
-  return (
-    <div>
-      App
-    </div>
-  )
-}
+const App = () => (
+  <BrowserRouter>
+    <Box>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Feed />} />
+        <Route path="/video/:id" element={<VideoDetail />} />
+        <Route path="/channel/:id" element={<ChannelDetail />} />
+        <Route path="search/:searchTerm" element={<SearchFeed />} />
+      </Routes>
+    </Box>
+  </BrowserRouter>
+);
 
-export default App
+export default App;
